0df62fdd7d260c6d16f5dc6a5ee6547246d6b169
suryadharma5 <suryadharmasetiawan03@gmail.com>
2023-12-14 23:50:45 +0700

    added: slicing home page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import logo from '../assets/logo.png'
 
 const Navbar = () => {
     return (
-        <div className='w-full flex max-h-30 px-9 py-1 space-x-5 items-center bg-slate-200'>
+        <div className='w-full flex max-h-30 px-9 py-1 space-x-5 items-center bg-slate-200 shadow-2xl sticky top-0 border-b-4 border-slate-300'>
             <img src={logo} alt="" className='h-20 mr-20' />
             <p>Home</p>
             <p>Furniture</p>
